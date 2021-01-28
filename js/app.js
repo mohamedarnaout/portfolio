4903966984a873a51e6c7b40b403ea54527d757a
git config --global user.name mohamed <mohamedearna@gmail.com>
2021-01-28 11:10:22 -0600

    new start

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let body = document.querySelector("body");
 let nav = document.querySelector("nav");
+let intFrameHeight = window.innerHeight;
 
 
 window.addEventListener('scroll', ()=>{
-    const scrolled = window.scrollY;
-    if(scrolled > 700){
+    const scroll = window.scrollY;
+    if(scroll >= intFrameHeight){
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
